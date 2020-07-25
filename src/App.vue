343db4ea508ad90c5cb4c59eb54ad9d9643a0934
terry <250263649@qq.com>
2020-07-25 14:40:41 +0800

    'addIMG'

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,5 @@
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    width: 100%;
-    color: #42b983;
   }
 </style>
