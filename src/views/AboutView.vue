148180963e8373d101af2bd1c0c491931bb63b48
chilltoo <chilltoo@gmail.com>
2024-10-01 12:52:15 +0800

    创建Vue3@next框架替换之前的版本

---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about pages</h1>
+  </div>
+</template>
+
+<!-- js start-->
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+<!-- js end -->
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
